45c63ce7560790dcb0bb343de22f3bd9d98dd228
Maher Patel <maherpatel07@gmail.com>
2026-08-13 19:11:56 +0530

    fix: correct insertion sort loop boundaries and element placement in the for and while loops

--- questions/question-02-task-priority.cpp
+++ questions/question-02-task-priority.cpp
@@ -5,18 +5,18 @@ using namespace std;
 
 void sortPriorities(int priority[], int n)
 {
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i < n; i++)
     {
         int key = priority[i];
         int j = i - 1;
 
-        while (j > 0 && priority[j] > key)
+        while (j >= 0 && priority[j] > key)
         {
             priority[j + 1] = priority[j];
             j--;
         }
 
-        priority[j] = key;
+        priority[j+1] = key;
     }
 }
 
